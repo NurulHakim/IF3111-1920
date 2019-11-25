ecf9e639facbc433d434be75aa28f03d71a340ad
NurulHakim <nurulhakim2810@gmail.com>
2019-11-25 17:31:59 +0700

    inisialisasi page akun

---
+++ application/views/akun/akun.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Akun</title>
+
+    <style media="screen">
+        body{
+          margin: 0px;
+        }
+
+        .sub a{
+            font-family: arial;
+            font-size : 15px;
+            text-decoration:none
+            
+        }
+        
+        .judul{
+          text-align: center;
+          padding-top: 50px;
+        }
+        #inline {
+            margin: 15px auto;
+            padding: 0;
+        }
+
+        #inline li {
+            display: inline;
+            margin-left: 15px;
+            margin-right: 15px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="judul">
+        <h1>SIMPLE LAPOR!</h1>
+    </div>
+    <div class="sub">
+        <ul id="inline">
+            <li><a href="">Lihat Laporan</a></li>
+            <li><a href="">Buat Laporan</a></li>
+            <li><a href="">Edit Laporan</a></li>
+        </ul>
+        <hr width="28%" align="left">
+    </div>
+    
+</body>
+</html>
