7968a30b18861b598ffa8d6a5db5dce7a365eb8b
VirdannisaSelqy <virdanisa.sathpira@gmail.com>
2019-12-13 17:09:41 +0700

    Buat_Laporan

---
+++ application/views/Buat_laporan/Buat_Laporan.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Buat Laporan</title>
+</head>
+
+<body>
+    <div class="judul">
+        <h1>SIMPLE LAPOR!</h1>
+    </div>
+    <br>
+    <div>
+        <p class="Laporan_Komentar">Buat Laporan/Komentar</p>
+    </div>
+    <div class="Kotak_Komentar">
+        <textarea name="Laporan/Komentar" rows="25" cols="100">Laporan/Komentar</textarea>
+    </div>
+    <div class="Aspek">
+        <select name="Pilih Aspek Pelaporan/Komentar">
+            <option>Keluhan sarana dan prasarana</option>
+        </select>
+
+    </div>
+</body>
+
+</html>
