5edb9637cf1508b370bc97f70bd2270d6cf86b86
VirdannisaSelqy <virdanisa.sathpira@gmail.com>
2019-12-14 11:17:32 +0700

    update halaman buat laporan

--- application/views/Buat_laporan/Buat_Laporan.php
+++ application/views/Buat_laporan/Buat_Laporan.php
@@ -21,25 +21,27 @@
         <hr />
     </div>
     <br>
-    <div class="Kotak_Komentar">
-        <textarea name="Laporan/Komentar" rows="18" cols="110">Laporan/Komentar</textarea>
-    </div>
-    <div class="Aspek">
-        <select>
-            <option value="Keluhan1">Keluhan tentang sarana dan prasarana </option>
-            <option value="Keluhan2">Keluhan tentang proses pembuatan surat perizinan </option>
-            <option value="Keluhan3">Keluhan tentang proses pengajaran </option>
-            <option value="Keluhan4">Keluhan tentang proses penilaian </option>
-        </select>
-    </div>
-    <div class="form">
-        <form action="" method="post" enctype="multipart/form-data">
-            <input type="file" name="upload" class="upload">
-        </form>
-    </div>
-    <div>
-        <input type="button" value="Buat LAPOR!" class="tombol">
-    </div>
+    <form action="<?php echo base_url() . "Buat_laporan/kirim_L" ?>" method="post" enctype="multipart/form-data">
+        <div class="Kotak_Komentar">
+            <textarea name="Komentar" rows="18" cols="110">Laporan/Komentar</textarea>
+        </div>
+        <div class="Aspek">
+            <select name="aspek">
+                <option value="Keluhan1">Keluhan tentang sarana dan prasarana </option>
+                <option value="Keluhan2">Keluhan tentang proses pembuatan surat perizinan </option>
+                <option value="Keluhan3">Keluhan tentang proses pengajaran </option>
+                <option value="Keluhan4">Keluhan tentang proses penilaian </option>
+            </select>
+        </div>
+        <div class="form">
+
+            <input type="file" name="lampiran" class="upload">
+
+        </div>
+        <div>
+            <input type="submit" value="Buat LAPOR!" class="tombol">
+        </div>
+    </form>
     <br>
     <div class="garis">
         <hr />
