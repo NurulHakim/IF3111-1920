040e3fe3287f76423320d8cae59d9c4d2aadda80
revtm <kykylizaro98@gmail.com>
2019-12-14 21:52:37 +0700

    tampil gambar dan download dokumen

--- application/views/lihat_selengkapnya.php
+++ application/views/lihat_selengkapnya.php
@@ -1,134 +1,151 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lihat Selengkapnya</title>
-    <style media="screen">
-        .judul{
-          text-align: center;
-          padding-top: 50px;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Lihat Selengkapnya</title>
+  <style media="screen">
+    .judul {
+      text-align: center;
+      padding-top: 50px;
+    }
 
-        .detail{
-            padding-left: 30px;
-        }
-        
-        body{
-          margin: 0px;
-        }
-        
+    .detail {
+      padding-left: 30px;
+    }
 
-        .container_lihat_selengkapnya{
-          padding-top: 20px;
-          padding-left: 30px;
-          padding-right: 30px;
-        }
+    body {
+      margin: 0px;
+    }
 
-        .highlight_laporan{
-          padding-top: 20px;
-          padding-bottom: 50px;
-        }
 
-        .waktu_laporan{
-          text-align: left;
-        }
+    .container_lihat_selengkapnya {
+      padding-top: 20px;
+      padding-left: 30px;
+      padding-right: 30px;
+    }
 
-        #lihat{
-          padding-left: 30px;
-        }
+    .highlight_laporan {
+      padding-top: 20px;
+      padding-bottom: 50px;
+    }
 
-        .selengkapnya{
-          text-align: left;
+    .waktu_laporan {
+      text-align: left;
+    }
 
-    
-        }
-        .hapus{
-          text-align: right;
-          text-decoration: none;
-        }
+    #lihat {
+      padding-left: 30px;
+    }
 
-        .edit{
-          text-align: right;
-          text-decoration: none;
-        }
+    .selengkapnya {
+      text-align: left;
 
-        table{
-          width: 1100px;
-        }
 
-    </style>
+    }
+
+    .hapus {
+      text-align: right;
+      text-decoration: none;
+    }
+
+    .edit {
+      text-align: right;
+      text-decoration: none;
+    }
+
+    table {
+      width: 1100px;
+    }
+  </style>
 
 
 
 </head>
+
 <body>
-    <div class="judul">
-        <h1>SIMPLE LAPOR!</h1>
-    </div>
+  <div class="judul">
+    <h1>SIMPLE LAPOR!</h1>
+  </div>
 
-    <div class="detail">
-        <h3>Detail Laporan</h3>
-    </div>
-    <div class="container_lihat_selengkapnya">
+  <div class="detail">
+    <h3>Detail Laporan</h3>
+  </div>
+  <div class="container_lihat_selengkapnya">
 
-          <?php
-            foreach($laporan as $x){
-           ?>
-           <hr>
-            <table>
-              <tr>
-                  <td colspan="4">
-                    <div class="highlight_laporan">
-                      <?php echo $x->isi_laporan; ?>
-                    </div>
-                  </td>
-              </tr>
-              <tr>
-                <td colspan="4">
-                  <div class="lampiran_laporan">
-                      Lampiran: <a href="<?php echo $x->lampiran; ?>" id="judul_lampiran"><?php echo $x->lampiran; ?></a>
-                  </div>
-                </td>
-                </tr>
+    <?php
+    foreach ($laporan as $x) {
+      ?>
+      <hr>
+      <table>
+        <tr>
+          <td colspan="4">
+            <div class="highlight_laporan">
+              <?php echo $x->isi_laporan; ?>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="4">
+            <div class="lampiran_laporan">
+              <?php
+                $arr = explode('.', $x->lampiran);
+                $get_extensi = end($arr);
 
-                <tr>
-                    <td>
-                    <div class="selengkapnya">
-                        Waktu: <span id="tanggal_laporan"><?php echo $x->tanggal; ?></span>
-                    </div>
-                    </td>
-                    <td>
-                    <div class="selengkapnya">
-                        Aspek: <span id="aspek_laporan"><?php echo $x->aspek; ?></span>
-                    </div>
-                    </td>
+                if ($get_extensi == "png" || $get_extensi == "jpg" || $get_extensi == "jpeg") {
+                  ?>
 
-                    <td>
-                    <div class="edit">
-                        <!-- <a href="">Edit Laporan</a> -->
-                        <?php echo anchor('Lihat_selengkapnya/edit_laporan/'.$x->id_laporan,'Edit Laporan'); ?>
+                <img width="300" src=<?php echo base_url() . "assets/upload/" . $x->lampiran; ?> alt="xxx.jpg">
+              <?php
+                } else {
+                  ?>
+                Lampiran: <a href="<?php echo base_url() . "assets/upload/" . $x->lampiran;  ?>" id="judul_lampiran"><?php echo $x->lampiran; ?></a>
+              <?php
+                }
+                ?>
+            </div>
+          </td>
+        </tr>
 
-                        
-                      </div>
-                    </td>
+        <tr>
+          <td>
+            <div class="selengkapnya">
+              Waktu: <span id="tanggal_laporan"><?php echo $x->tanggal; ?></span>
+            </div>
+          </td>
+          <td>
+            <div class="selengkapnya">
+              Aspek: <span id="aspek_laporan"><?php echo $x->aspek; ?></span>
+            </div>
+          </td>
 
-                    <td>
-                    <div class="hapus" onclick="return confirm('Apakah kamu yakin ingin menghapus laporan?')">
-                        <!-- <a href=  >Hapus Laporan/Komentar<b> X</b></a> -->
-                        <?php echo anchor('Lihat_selengkapnya/hapus/'.$x->id_laporan,'Hapus Laporan/Komentar'); ?>
-                      </div>
-                    </td>
-
-                    
-              </tr>
-
-
-            </table>
-          <?php } ?>
+          <td>
+            <div class="edit">
+              <!-- <a href="">Edit Laporan</a> -->
+              <?php echo anchor('Lihat_selengkapnya/edit_laporan/' . $x->id_laporan, 'Edit Laporan'); ?>
 
 
-        </div>
+            </div>
+          </td>
+
+          <td>
+            <div class="hapus" onclick="return confirm('Apakah kamu yakin ingin menghapus laporan?')">
+              <!-- <a href=  >Hapus Laporan/Komentar<b> X</b></a> -->
+              <?php echo anchor('Lihat_selengkapnya/hapus/' . $x->id_laporan, 'Hapus Laporan/Komentar'); ?>
+            </div>
+          </td>
+
+
+        </tr>
+
+
+      </table>
+    <?php } ?>
+
+
+  </div>
 </body>
+
 </html>
